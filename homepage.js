b2d9d35d7fc6f4c1f089e970bb38accd996b4620
Shiva Jadiya <shivajadiya2005@gmail.com>
2025-05-07 14:01:25 +0530

    Added homepage code

---
+++ homepage.js
@@ -0,0 +1,39 @@
+gsap.from("#Heading ,h1,h4",{
+    duration:2,
+    opacity:0,
+    delay:0.5,
+    Y:1200,
+    })
+
+    gsap.from("#head_btn",{
+              duration:2,
+              x:1300,
+              delay:1,
+              opacity:0,
+              
+        })
+
+        gsap.from("#card1",{
+            duration:1,
+            x:1300,
+            delay:3,
+            opacity:0,
+            stagger:1
+           
+           
+      })
+      gsap.from("#card2",{
+        duration:2,
+        y:1300,
+        delay:3,
+        opacity:0,
+        stagger:1 
+  })
+  gsap.from("#card3",{
+    duration:2,
+    x:1300,
+    delay:3,
+    opacity:0,
+    stagger:1
+})
+
