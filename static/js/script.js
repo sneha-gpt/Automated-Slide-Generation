802edd840c1bf805fac5a965d5ec0250e394059f
vaidik jaiswal <vaidikjaiswal@gmail.com>
2025-05-25 19:47:03 +0530

    redone firebase

--- static/js/script.js
+++ static/js/script.js
@@ -13,15 +13,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Firebase configuration
   const firebaseConfig = {
-  
-          'apiKey': os.getenv('FIREBASE_API_KEY'),
-          'authDomain': os.getenv('FIREBASE_AUTH_DOMAIN'),
-          'projectId': os.getenv('FIREBASE_PROJECT_ID'),
-          'storageBucket': os.getenv('FIREBASE_STORAGE_BUCKET'),
-          'messagingSenderId': os.getenv('FIREBASE_MESSAGING_SENDER_ID'),
-          'appId': os.getenv('FIREBASE_APP_ID'),
-          'measurementId': os.getenv('FIREBASE_MEASUREMENT_ID')
-
+    apiKey: "AIzaSyAzE0EmATnlMqFooV1aNCU0h1c1wsGBEN0",
+    authDomain: "automated-slide-generation.firebaseapp.com",
+    projectId: "automated-slide-generation",
+    storageBucket: "automated-slide-generation.firebasestorage.app",
+    messagingSenderId: "262134603450",
+    appId: "1:262134603450:web:049dd5b95a121798e76c83",
+    measurementId: "G-5D502BDDND"
   };
 
   // Initialize Firebase
