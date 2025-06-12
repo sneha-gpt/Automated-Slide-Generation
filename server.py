8bc2744b71de74a4477f537526c5bffdb85f31a9
Shiva Jadiya <shivajadiya2005@gmail.com>
2025-06-12 20:25:08 +0530

    orinal chnge

--- server.py
+++ server.py
@@ -304,9 +304,5 @@ def process_document():
 
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
-
-#if __name__ == '__main__':
-    #app.run(debug=True, port=8080)
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
