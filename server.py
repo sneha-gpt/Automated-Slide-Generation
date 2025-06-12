68610511f5dade0c49ad9734a30cd4de32a548a8
Shiva Jadiya <shivajadiya2005@gmail.com>
2025-06-12 20:02:52 +0530

    change only port server e

--- server.py
+++ server.py
@@ -302,5 +302,11 @@ def process_document():
     except Exception as e:
         return jsonify({'filename': filename, 'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+#if __name__ == '__main__':
+    #app.run(debug=True, port=8080)
